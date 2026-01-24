a4a20e5adcd87eb0317946d2e4f84528829b96f6
Jonah Ssegawa <jonahssegawa44532@gmail.com>
2026-01-24 14:46:08 +0300

    initial commit

---
+++ devtools/app.js
@@ -0,0 +1,18 @@
+  requestAnimationFrame(() => {
+        const start = performance.now();
+        
+
+        while (performance.now() - start < 2000){
+
+            console.log(start);
+            
+            
+        }
+
+    });  
+
+function jhex(){
+    const doNothing = 2;
+}
+
+
